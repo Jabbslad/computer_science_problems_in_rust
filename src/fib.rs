24a72b58ed4d1b4dc92503125236b0f6e771b077
Jamie Atkinson <jabbslad@gmail.com>
2020-06-14 13:17:29 +0100

    add iterative fibonacci

--- src/fib.rs
+++ src/fib.rs
@@ -19,6 +19,20 @@ pub fn fib_mem(n: i64) -> i64 {
     fib_m(&mut m, n)
 }
 
+pub fn fib_iter(n: i64) -> i64 {
+    if n == 0 {
+        return n;
+    }
+    let mut last = 0;
+    let mut next = 1;
+    for _ in 1..n {
+        let mut tmp = last;
+        last = next;
+        next = tmp + next
+    }
+    next
+}
+
 fn helper(a: i64, b: i64, n: i64) -> i64 {
     if n > 0 {
         helper(b, a + b, n - 1)
@@ -43,13 +57,14 @@ pub fn fib_no_tr(n: i64) -> i64 {
 #[cfg(test)]
 mod tests {
 
-    use crate::fib::{fib, fib_no_tr, fib_mem};
+    use crate::fib::*;
 
     #[test]
     fn fib0() {
         assert_eq!(0, fib(0));
         assert_eq!(0, fib_no_tr(0));
         assert_eq!(0, fib_mem(0));
+        assert_eq!(0, fib_iter(0));
     }
 
     #[test]
@@ -57,7 +72,7 @@ mod tests {
         assert_eq!(1, fib(1));
         assert_eq!(1, fib_no_tr(1));
         assert_eq!(1, fib_mem(1));
-
+        assert_eq!(1, fib_iter(1));
     }
 
     #[test]
@@ -65,11 +80,13 @@ mod tests {
         assert_eq!(55, fib(10));
         assert_eq!(55, fib_no_tr(10));
         assert_eq!(55, fib_mem(10));
+        assert_eq!(55, fib_iter(10));
     }
 
     #[test]
     fn fib250() {
         assert_eq!(12586269025, fib(50));
         assert_eq!(12586269025, fib_mem(50));
+        assert_eq!(12586269025, fib_iter(50));
     }
 }
